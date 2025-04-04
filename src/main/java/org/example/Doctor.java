b23cc010be4b66f3992eaa3686d6926f76a6a744
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-04-04 14:59:14 -0500

    subiendo modificaciones para agregar clases anidadas y metodos anidados

--- src/main/java/org/example/Doctor.java
+++ src/main/java/org/example/Doctor.java
@@ -1,10 +1,14 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+
 public class Doctor {
     static int id = 1; //Autoincremental
-    String name;
-    String speciality;
-    String email;
+    private String name;
+    private String speciality;
+    private String email;
 
     Doctor(){
         System.out.println("construyendo el objeto doctor");
@@ -24,4 +28,52 @@ public class Doctor {
     public void showId(){
         System.out.println("ID Doctor: "+ id);
     }
+
+    ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
+    public void addAvailableAppointment(Date date, String time){
+        availableAppointments.add(new Doctor.AvailableAppointment(date, time));
+    }
+
+    public ArrayList<AvailableAppointment> getAvailableAppointments(){
+        return  availableAppointments;
+    }
+
+    public  static class AvailableAppointment{
+        private int id;
+        private Date date;
+        private String time;
+        //Available_appoinment
+
+        public AvailableAppointment(Date date, String time) {
+            this.date = date;
+            this.time = time;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
+    }
 }
+
+
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,29 @@
 package org.example;
 
+import java.util.Date;
+
 import static org.example.ui.UIMenu.showMenu;
 
 public class Main {
     public static void main(String[] args) {
 
 //        showMenu();
-
+//
         Doctor myDoctor = new Doctor("Ana gonzalez", "pediatria");
-        System.out.println(myDoctor.name);
-        System.out.println(myDoctor.speciality);
+//        System.out.println(myDoctor.name);
+//        System.out.println(myDoctor.speciality);
 
+        myDoctor.addAvailableAppointment(new Date(), "4pm");
+        myDoctor.addAvailableAppointment(new Date(), "10am");
+        myDoctor.addAvailableAppointment(new Date(), "1pm");
 
-        Patient patient = new Patient("Alejandra", "aleja@gmail.com");
-        Patient patient2 = new Patient("anahi", "ana@gmail.com");
+        for (Doctor.AvailableAppointment aA: myDoctor.getAvailableAppointments()){
+            System.out.println(aA.getDate()+ " " + aA.getTime());
+        }
+
+//
+//        Patient patient = new Patient("Alejandra", "aleja@gmail.com");
+//        Patient patient2 = new Patient("anahi", "ana@gmail.com");
 
 //        System.out.println(patient.getName());
 //        System.out.println(patient2.getName());
@@ -23,11 +33,11 @@ public class Main {
 //        System.out.println(patient.getName());
 //        System.out.println(patient2.getName());
 
-        patient.setWeight(54.6);
-        System.out.println(patient.getWeight());
-
-        patient.setPhoneNumber("12345678");
-        System.out.println(patient.getPhoneNumber());
+//        patient.setWeight(54.6);
+//        System.out.println(patient.getWeight());
+//
+//        patient.setPhoneNumber("12345678");
+//        System.out.println(patient.getPhoneNumber());
 
 
 
