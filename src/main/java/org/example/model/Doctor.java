d32b8dee9ee4ed6c0340a14303b3d172288d0fbf
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-04-07 11:31:08 -0500

    subiendo nueva interfaz de doctores, realizando las respectivas validaciones

--- src/main/java/org/example/model/Doctor.java
+++ src/main/java/org/example/model/Doctor.java
@@ -1,5 +1,7 @@
 package org.example.model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -28,7 +30,9 @@ public class Doctor extends User {
 
 
     ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
-    public void addAvailableAppointment(Date date, String time){
+    public void addAvailableAppointment(String date, String time){
+
+
         availableAppointments.add(new Doctor.AvailableAppointment(date,time));
     }
 
@@ -46,11 +50,16 @@ public class Doctor extends User {
         private int id;
         private Date date;
         private String time;
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM//yyyy");
 
 //Available_appoinment
 
-        public AvailableAppointment(Date date, String time) {
-            this.date = date;
+        public AvailableAppointment(String date, String time) {
+            try {
+                this.date = format.parse(date);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
             this.time = time;
         }
 
@@ -64,6 +73,10 @@ public class Doctor extends User {
 
         public Date getDate() {
             return date;
+        }
+
+        public String getDate(String DATE) {
+            return format.format(date);
         }
 
         public void setDate(Date date) {
